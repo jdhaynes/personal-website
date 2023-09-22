8b416067fe9ef442eca3083f93c463a2be4d3950
Jack Haynes <jackhaynes94@gmail.com>
2023-09-22 20:26:33 +0100

    Fixed formatting issue with markdown code blogs and made full width.

--- components/blog/Post.tsx
+++ components/blog/Post.tsx
@@ -6,13 +6,17 @@ import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 
+const CodePre: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
 const Post: React.FC<Post> = ({ metadata, content }) => {
   return (
     <article className="flex flex-col space-y-2">
       <h1 className="font-bold text-4xl text-center">{metadata.title}</h1>
       <section className="text-xl text-center">{metadata.subtitle}</section>
       <ReactMarkdown
-        className="prose"
+        className="prose max-w-none"
         children={content}
         components={{
           code({ node, inline, className, children, ...props }) {
@@ -23,7 +27,7 @@ const Post: React.FC<Post> = ({ metadata, content }) => {
                 children={String(children).replace(/\n$/, "")}
                 style={vscDarkPlus}
                 language={match[1]}
-                PreTag="div"
+                PreTag={CodePre}
               />
             ) : (
               <code {...props} className={className}>
